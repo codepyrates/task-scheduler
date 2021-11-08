a5bf5a11c61a0190a0ada750be670328e1385313
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 16:32:08 +0200

    test: added a test to check the number of rows in the reminders instance variable of a reminder handler instance

--- tests/test_reminders/test_reminder/test_reminder.py
+++ tests/test_reminders/test_reminder/test_reminder.py
@@ -3,3 +3,12 @@ def test_initializing_a_reminder_handler_instance():
     rmh = ReminderHandler("tests/test_reminder/reminder.csv")
     actual = rmh.__class__.__name__
     assert actual == expected    
+
+def test_number_of_rows_in_reminders_instance_variable():
+    expected = 2
+    rmh = ReminderHandler("tests/test_reminder/reminder.csv")
+    actual = len(rmh.reminders)
+    assert actual == expected
+    
+
+    
