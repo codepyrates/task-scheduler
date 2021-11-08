aaeddfb0a577c30bf50f60b04d23b7374031214e
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 21:17:37 +0200

    test: added test_delete_reminder_scenario_1

--- tests/test_reminders/test_reminders_delete_reminders/test_delete_reminder.py
+++ tests/test_reminders/test_reminders_delete_reminders/test_delete_reminder.py
@@ -1,5 +1,6 @@
 import pytest
 from task_scheduler.reminders.reminder_handler import RemindersHandler
+from tests.test_reminders.reminders_mocker import reminder_mocker
 
 @pytest.fixture
 def reminder_handler():
@@ -19,3 +20,6 @@ def test_delete_reminder_by_value_of_remaining_reminders(reminder_handler):
     actual = reminder_handler.reminders.iloc[0]['message']
     assert actual == expected
     
+def  test_delete_reminder_scenario_1(reminder_handler):
+    diffs = reminder_mocker(reminder_handler.start, path="tests/test_reminders/test_delete_reminder/delete_reminder_scenario_1.txt")
+    assert not diffs, diffs
