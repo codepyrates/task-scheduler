9670716eff4f93e44f3c82fbd6aa2b2046a35fd0
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 17:12:09 +0200

    feat: added the ReminderHandler class and its constructor

---
+++ task_scheduler/reminder/reminder_handler.py
@@ -0,0 +1,9 @@
+import pandas as pd
+class RemindersHandler:
+    def __init__(self, path):
+        self.path = path
+        self.next_reminder = {"time": None,"date":None, "message" : None}
+        self.reminders = pd.read_csv(path)
+        self.reminders['time'] = pd.to_datetime(self.reminders['time'], format="%Y-%m-%d %H:%M:%S")
+        self.sort_reminders()
+        self.update_next_reminder()
