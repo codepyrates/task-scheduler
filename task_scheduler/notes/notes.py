08e2879a4a64e266832864eed5df4571fb731627
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-11-08 21:50:05 +0200

    add view note feature

--- task_scheduler/notes/notes.py
+++ task_scheduler/notes/notes.py
@@ -1,5 +1,6 @@
 import csv
-# import pandas as pd
+import pandas as pd
+
 
 class Notes:
 
@@ -12,24 +13,42 @@ class Notes:
         self.content = new_content
     def __str__(self):
         return f"Title > {self.title} & Content > {self.content}"
-        
+
 class NotesHandler:
-    # def __init__(self):
-        # self.notes = pd.read_csv('./notes.csv')
+
+    def __init__(self):
+
+        self.notes = pd.read_csv('./notes.csv')
+
     def add_note(self, title, content):
         self.notes = self.notes.append({"Title": title, "Content": content}, ignore_index=True)
+
+    def view_notes(self):
+
+        records = list(self.notes.to_records(index=True))
+        for index, title, content in records:
+            print(f"{index}. {title}\n  {content}")
+
 
 
 if __name__ == "__main__":
 
-    nh = NotesHandler()
+    handler = NotesHandler()
+    
     while True:
+
         choice = input("a: Add new note, v: View notes, d: Delete note, m: modify, q: quit,  >>> ")
         if choice == "q": break
+
         elif choice == "a":
             title = input("Title of the new note >>> ")
             content = input("Content of the new note >>> ")
-            nh.add_note(title, content)
+            handler.add_note(title, content)
             print('Note added successfully!')
+
+        elif choice == "v":
+            handler.view_notes()
+        
+        
         else:
             print("Please choose one of the available choices")
