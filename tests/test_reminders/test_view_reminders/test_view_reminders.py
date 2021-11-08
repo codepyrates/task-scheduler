8beefcba29402fa18bf163f1a8d8d5460f757ad9
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 18:29:03 +0200

    test: added a fixture to return a reminder handler instane

---
+++ tests/test_reminders/test_view_reminders/test_view_reminders.py
@@ -0,0 +1,7 @@
+import pytest
+from task_scheduler.reminder.reminder_handler import RemindersHandler
+
+@pytest.fixture
+def reminder_handler():
+    rmh = RemindersHandler("./tests/test_reminders/reminder.csv")
+    return rmh
