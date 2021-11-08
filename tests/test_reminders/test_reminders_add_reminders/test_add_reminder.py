8fd545d4825c014ac24bbe0d5f362575cceabcbe
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 21:39:46 +0200

    fix: fixed add_reminder_scenario_1.txt

--- tests/test_reminders/test_reminders_add_reminders/test_add_reminder.py
+++ tests/test_reminders/test_reminders_add_reminders/test_add_reminder.py
@@ -3,7 +3,7 @@ import pytest
 from task_scheduler.reminders.reminder_handler import RemindersHandler
 from tests.test_reminders.reminders_mocker import reminder_mocker
 
-
+@pytest.fixture
 def reminder_handler():
     rmh = RemindersHandler("./tests/test_reminders/reminders.csv")
     return rmh
