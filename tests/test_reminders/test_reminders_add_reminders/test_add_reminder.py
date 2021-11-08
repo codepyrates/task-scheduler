56d449d1bfa6141796535d90696d2d728539ec17
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 20:30:41 +0200

    test: added a test to check add_reminder method

--- tests/test_reminders/test_reminders_add_reminders/test_add_reminder.py
+++ tests/test_reminders/test_reminders_add_reminders/test_add_reminder.py
@@ -6,3 +6,9 @@ from task_scheduler.reminders.reminder_handler import RemindersHandler
 def reminder_handler():
     rmh = RemindersHandler("./tests/test_reminders/reminders.csv")
     return rmh
+
+def test_add_reminder(reminder_handler):
+    expected = "latest reminder"
+    reminder_handler.add_reminder("2022-12-31 22:22:22", "latest reminder")
+    actual = reminder_handler.reminders.iloc[2]['message']
+    assert actual == expected
--- tests/test_reminders/test_reminders_update_reminders/test_update_reminder.py
+++ tests/test_reminders/test_reminders_update_reminders/test_update_reminder.py
@@ -1,6 +1,5 @@
 import pytest
 from task_scheduler.reminder.reminder_handler import RemindersHandler
-from tests.test_reminders.reminders_mocker import reminder_mocker
 
 @pytest.fixture
 def reminder_handler():
