dc2caf7984f04cdb0fa220051bcf1c1332a7c762
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 16:27:08 +0200

    test: added a test for checking instantiating a new reminder handler instance

--- tests/test_reminders/test_reminder/test_reminder.py
+++ tests/test_reminders/test_reminder/test_reminder.py
@@ -1,2 +1,5 @@
-def test_reminder_instance():
-    
+def test_initializing_a_reminder_handler_instance():
+    expected = "ReminderHandler"
+    rmh = ReminderHandler("tests/test_reminder/reminder.csv")
+    actual = rmh.__class__.__name__
+    assert actual == expected    
