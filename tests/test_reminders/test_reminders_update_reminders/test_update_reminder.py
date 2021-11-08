08e53bf267b06828424e1c20c46a1f9dc0d8676b
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 20:18:17 +0200

    test: added reminder_handler fixture

---
+++ tests/test_reminders/test_reminders_update_reminders/test_update_reminder.py
@@ -0,0 +1,8 @@
+import pytest
+from task_scheduler.reminder.reminder_handler import RemindersHandler
+from tests.test_reminders.reminders_mocker import reminder_mocker
+
+@pytest.fixture
+def reminder_handler():
+    rmh = RemindersHandler("./tests/test_reminders/reminder.csv")
+    return rmh
