41cddaff301b501eef0b3e7dc62a24ea01a1c4ab
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 19:57:48 +0200

    test: added a test to check the delete_reminder method scenario 1

--- tests/test_reminders/test_reminders_delete_reminders/test_delete_reminder.py
+++ tests/test_reminders/test_reminders_delete_reminders/test_delete_reminder.py
@@ -7,4 +7,6 @@ def reminder_handler():
     rmh = RemindersHandler("./tests/test_reminders/reminder.csv")
     return rmh
 
-
+def  test_delete_reminder_scenario_1(reminder_handler):
+    diffs = reminder_mocker(reminder_handler.start, path="tests/test_reminders/test_delete_reminder/delete_reminder_scenario_1.txt")
+    assert not diffs, diffs
