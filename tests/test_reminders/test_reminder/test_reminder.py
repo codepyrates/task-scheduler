c1c6aeff9579b1dfca398fc3fe1375327bfb0b63
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 16:46:06 +0200

    test: added a test to check if the time column in the reminders instance variable is datetime

--- tests/test_reminders/test_reminder/test_reminder.py
+++ tests/test_reminders/test_reminder/test_reminder.py
@@ -1,18 +1,26 @@
+import numpy as np
+
 def test_initializing_a_reminder_handler_instance():
     expected = "ReminderHandler"
-    rmh = ReminderHandler("tests/test_reminder/reminder.csv")
+    rmh = RemindersHandler("tests/test_reminder/reminder.csv")
     actual = rmh.__class__.__name__
     assert actual == expected    
 
 def test_number_of_rows_in_reminders_instance_variable():
     expected = 2
-    rmh = ReminderHandler("tests/test_reminder/reminder.csv")
+    rmh = RemindersHandler("tests/test_reminder/reminder.csv")
     actual = len(rmh.reminders)
     assert actual == expected
     
 def test_number_of_columns_in_reminders_instance_variable():
     expected = 2
-    rmh = ReminderHandler("tests/test_reminder/reminder.csv")
+    rmh = RemindersHandler("tests/test_reminder/reminder.csv")
     actual = len(rmh.reminders.columns)
+    assert actual == expected
+    
+def test_dtype_of_time_column_reminders_instance_variable_is_datetime():
+    expected = np.datetime64
+    rmh = ReminderHandler("tests/test_reminder/reminder.csv")
+    actual = rmh.reminders.time.dtype
     assert actual == expected
     
