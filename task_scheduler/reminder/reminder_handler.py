07bcd8bacd75500e2e527b4c073d14b09e7779d4
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 17:13:05 +0200

    feat: added the sort_reminders method

--- task_scheduler/reminder/reminder_handler.py
+++ task_scheduler/reminder/reminder_handler.py
@@ -7,3 +7,5 @@ class RemindersHandler:
         self.reminders['time'] = pd.to_datetime(self.reminders['time'], format="%Y-%m-%d %H:%M:%S")
         self.sort_reminders()
         self.update_next_reminder()
+    def sort_reminders(self):
+        self.reminders.sort_values('time', inplace=True, ignore_index=True,  ascending=True)
