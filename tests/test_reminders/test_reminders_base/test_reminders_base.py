0684972fbb18a9ef38bd5e882f7ecf6132e5b502
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 19:45:12 +0200

    test: added a test to check intializing a reminder handler instance

--- tests/test_reminders/test_reminders_base/test_reminders_base.py
+++ tests/test_reminders/test_reminders_base/test_reminders_base.py
@@ -6,3 +6,8 @@ from task_scheduler.reminders.reminder_handler import RemindersHandler
 def reminder_handler():
     rmh = RemindersHandler("./tests/test_reminders/reminders.csv")
     return rmh
+
+def test_initializing_a_reminder_handler_instance(reminder_handler):
+    expected = "RemindersHandler"
+    actual = reminder_handler.__class__.__name__
+    assert actual == expected   
