a6280187a8477673efda792e8a78dc99bdd7351f
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-08 20:25:24 +0200

    test: added a test to check update reminder method

--- tests/test_reminders/test_reminders_update_reminders/test_update_reminder.py
+++ tests/test_reminders/test_reminders_update_reminders/test_update_reminder.py
@@ -6,3 +6,10 @@ from tests.test_reminders.reminders_mocker import reminder_mocker
 def reminder_handler():
     rmh = RemindersHandler("./tests/test_reminders/reminder.csv")
     return rmh
+
+def test_update_reminder(reminder_handler):
+    expected = "2021-11-11 11:11:11"
+    reminder_handler.update_reminder(0, "2021-11-11 11:11:11", "new message")
+    actual = str(reminder_handler.reminders.iloc[0]['time'])
+    assert actual == expected
+    
