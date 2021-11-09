8b51007da0e555d31738a3b34b1660caef284148
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-09 18:52:27 +0200

    feat: added welcome function to main

---
+++ task_scheduler/main/main.py
@@ -0,0 +1,28 @@
+from art import tprint
+from termcolor import colored
+import time
+import threading
+
+
+def welcome():
+    welcome = "Welcome, this is"
+    welcome = colored(welcome, 'cyan', attrs=['bold'])
+    msg = ""
+    for l in welcome:
+        print(msg, end="\r")
+        msg += l
+        time.sleep(0.05)
+    time.sleep(0.25)
+    print("\n")
+    tprint("Anton")
+    welcome = "your assistant and scheduler."
+    welcome = colored(welcome, 'cyan', attrs=['bold'])
+    msg = ""
+    for l in welcome:
+        print(msg, end="\r")
+        msg += l
+        time.sleep(0.05)
+    time.sleep(0.25)
+    print("\n")
+
+
