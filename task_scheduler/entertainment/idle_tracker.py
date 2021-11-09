09d0e4859297a8dac2d8404f88d5f11eee6e1d51
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-09 19:12:56 +0200

    feat: added idle_tracker function

--- task_scheduler/entertainment/idle_tracker.py
+++ task_scheduler/entertainment/idle_tracker.py
@@ -2,3 +2,19 @@ from pynput.mouse import Controller
 from termcolor import colored
 import time
 
+def idle_tracker():
+    mouse = Controller()
+    init = mouse.position
+    period = 0
+    while True:
+        time.sleep(0.1)
+        period += 1
+        final = mouse.position
+        print(final)
+        if (init == final) and (period > 70):
+            
+            idle_tracker()
+        elif init != final:
+            time.sleep(0.1)
+            init = final
+            period = 0  
