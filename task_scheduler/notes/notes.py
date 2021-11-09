63bec5759e786c8122bf62d91a21c3e0e026a324
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-11-09 20:48:52 +0200

    add modify feature

--- task_scheduler/notes/notes.py
+++ task_scheduler/notes/notes.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 
 
@@ -18,12 +17,17 @@ class NotesHandler:
 
     def __init__(self):
 
-        self.notes = pd.read_csv('./notes.csv')
+        self.notes = pd.read_csv('./task_scheduler/notes.csv')
 
     def add_note(self, title, content):
+
+        """ Function to add notes in the notes page """
+
         self.notes = self.notes.append({"Title": title, "Content": content}, ignore_index=True)
 
     def view_notes(self):
+
+        """ Function viewing notes saved in notes page """
 
         records = list(self.notes.to_records(index=True))
         for index, title, content in records:
@@ -33,6 +37,13 @@ class NotesHandler:
         self.notes.drop(index, inplace=True)
         self.notes.reset_index(drop=True,inplace=True)
 
+    def modify_note(self, index, newtitle, newcontent):
+
+        """ Function modifying notes in notes page"""
+
+        self.notes.iloc[index]['title'] = newtitle
+        self.notes.iloc[index]['content'] = newcontent
+
 
 if __name__ == "__main__":
 
@@ -40,7 +51,11 @@ if __name__ == "__main__":
     
     while True:
 
-        choice = input("a: Add new note, v: View notes, d: Delete note, m: Modify note, Q: quit,  >>> ")
+        print('************** Notes Page ****************')
+        print('Please choose one of the following to do')
+
+        choice = input("a: Add new note, v: View notes, d: Delete note, m: Modify note, q: Quit,  >>> ")
+
         if choice == "q": break
 
         elif choice == "a":
@@ -56,6 +71,13 @@ if __name__ == "__main__":
             num = input("Enter the number of the note you want to delete >>> ")
             handler.delete_note(int(num))
             print(f"Note number {num} has been deleted successfully!")
+
+        elif choice == 'm':
+            index = int(input("enter the index of the note >>>"))
+            newtitle = input(f"enter the new title >>>")
+            newcontent = input(f"enter the new content >>>")
+            handler.modify_note(index, newtitle, newcontent)
+            print(f"note number {index} has been modified successfully!" )
         
         else:
             print("Please choose one of the available choices")
